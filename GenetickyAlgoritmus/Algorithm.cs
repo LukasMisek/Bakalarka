9b25d0887dfdc3db849ab66c7c8999b95951d439
LukasMisek <52196857+LukasMisek@users.noreply.github.com>
2020-01-08 18:09:12 +0100

    Update Algorithm.cs

--- GenetickyAlgoritmus/Algorithm.cs
+++ GenetickyAlgoritmus/Algorithm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -8,28 +8,78 @@ namespace GenetickyAlgoritmus
     public class Algorithm
     {
         // Velikost populace (počet jedinců)
-        public int POPULATION_SIZE = 10;
+        public static int POPULATION_SIZE = 10;
 
         // Počet generací (Počet cyklů)
-        public int GENERATION_COUNT = 5;
+        public static int GENERATION_COUNT = 10;
 
         // Šance ke křížení (80 = 80%)
-        public int P_CROSSOVER = 80;
+        public static int P_CROSSOVER = 80;
 
         // Šance k mutaci (10 = 10%)
-        public int P_MUTATION = 10;
+        public static int P_MUTATION = 10;
 
         // Délka genů (počet alel/genů/měst)
-        public int LENGTH = 80;
+        public static int LENGTH = 80;
 
         // Bod křížení (10 = na místě 10. jedince provedu křížení)
-        public int CROSSIN_POINT = 5;
+        public static int CROSSIN_POINT = 5;
 
         // Císlová vzdálenost mezi všemi městy (jak jedna vzdálenost je menší než cílová, tak algoritmus končí)
-        public double GOAL_DISTANCE = 0;
+        public static double GOAL_DISTANCE = 0;
 
         // Města mezi kterými počítám vzdálenosti
-        public string[] cities;
+        public static string[] cities;
+
+        public Algorithm(string[] s)
+        {
+                LENGTH = s.Length;
+                CROSSIN_POINT = s.Length / 2;
+                cities = s;
+        }
+
+
+        public Invidual start()
+
+        {
+            // Objekt s městy a maticí vzdáleností
+            /*
+            cities.showMatrix();
+            Console.WriteLine("mesta jsem udelal uspesne");
+            Console.ReadLine();
+            */
+            Population population = new Population();
+            // Objekt s populací    
+
+            /*
+            Console.WriteLine("populaci jsem udelal uspesne");
+            population.showMe();
+            Console.ReadLine();
+            */
+
+            if (cities.Length == 1)
+            {
+                return population.GetBest();
+            }
+
+
+            for (int i = 0; i < Algorithm.GENERATION_COUNT; i++)
+            {
+
+                population.improve();
+
+                /*
+                Console.WriteLine("Generation = " + i);
+                population.showMe();
+                */
+
+                if (algorithmEnd(population.GetBest().getDistance(), GOAL_DISTANCE)) break;
+
+            }
+
+            return population.GetBest();
+
+        }
 
         /// <summary>
         /// Ukončovací funkce
@@ -39,7 +89,7 @@ namespace GenetickyAlgoritmus
         /// <param name="bestDistance"></param>
         /// <param name="goalDistance"></param>
         /// <returns></returns>
-        public static bool algorithmEnd(double bestDistance, double goalDistance)
+        private static bool algorithmEnd(double bestDistance, double goalDistance)
         {
             if (bestDistance <= goalDistance) return true;
             else return false;
@@ -76,6 +126,7 @@ namespace GenetickyAlgoritmus
         {
             return cities;
         }
+
 
     }
 }
